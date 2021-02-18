79c618f7b50bec63c586aaa699ecf65440745af3
pfrlv <pfrlvgg@gmail.com>
2021-02-19 00:44:32 +0300

    Init 👏

---
+++ README.md
@@ -0,0 +1,13 @@
+<a href="https://whiterussian.studio/">
+  <img src="https://whiterussian.studio/assets/wrs.png" alt="White Russian Studio" width="48">
+</a>
+
+three.js starter
+===============================================
+
+1. Download
+2. `npm install`
+3. `npm run dev` — start develompent server and autorefresh your public/index.js
+4. `npm run prod` — create production version of your public/index.js
+
+All images, fonts & other assets save to `public` folder
---
+++ src/index.js
@@ -0,0 +1,44 @@
+import * as THREE from 'three';
+
+/**
+ * All available modules from https://github.com/mrdoob/three.js/tree/dev/examples/jsm
+ * use as
+ * import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+ * or
+ * import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js'
+ */
+
+let camera, scene, renderer
+let geometry, material, mesh
+
+init()
+
+function init() {
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    10
+  )
+  camera.position.z = 1
+
+  scene = new THREE.Scene()
+
+  geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
+  material = new THREE.MeshNormalMaterial()
+
+  mesh = new THREE.Mesh(geometry, material)
+  scene.add(mesh)
+
+  renderer = new THREE.WebGLRenderer({ antialias: true })
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setAnimationLoop(animation)
+  document.body.appendChild(renderer.domElement)
+}
+
+function animation(time) {
+  mesh.rotation.x = time / 2000
+  mesh.rotation.y = time / 1000
+
+  renderer.render(scene, camera)
+}
